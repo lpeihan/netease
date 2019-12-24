35ca1885ac7a8b01db888e381a20a139a160c907
李培汉 <lipeihan@chandashi.com>
2019-12-24 14:06:58 +0800

    feat: add less mixins

--- src/vant.ts
+++ src/vant.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
-import { Button } from "vant";
+import { Button, Toast } from "vant";
 
 Vue.use(Button);
+Vue.use(Toast);
---
+++ src/views/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img
+      v-for="banner in banners"
+      :src="banner.imageUrl"
+      :key="banner.imageUrl"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { getBanners } from "../../api/recommends";
+
+@Component
+export default class extends Vue {
+  banners: any[] = [];
+
+  title: string = "helo";
+
+  async getBanners() {
+    const res = await getBanners();
+
+    this.banners = res.data.banners;
+  }
+
+  created() {
+    this.getBanners();
+  }
+}
+</script>
