07138d3ab0af4e61340d6f989ed44a9706f2bdbb
李培汉 <lipeihan@chandashi.com>
2019-12-27 14:39:54 +0800

    perf: add storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,54 @@
+type StoreType = "localStorage" | "sessionStorage";
+
+class Store {
+  store: any;
+
+  constructor(type: StoreType) {
+    this.store = window[type];
+  }
+
+  serialize(val: any) {
+    return JSON.stringify(val);
+  }
+
+  deserialize(val: any) {
+    try {
+      return JSON.parse(val);
+    } catch (e) {
+      return val || undefined;
+    }
+  }
+
+  setItem(key: string, val: any) {
+    this.store.setItem(key, this.serialize(val));
+  }
+
+  getItem(key: string) {
+    return this.deserialize(this.store.getItem(key));
+  }
+
+  removeItem(key: string) {
+    this.store.removeItem(key);
+  }
+
+  clear() {
+    this.store.clear();
+  }
+
+  getAll() {
+    const store = this.store;
+    const ret: { [propName: string]: string } = {};
+
+    for (let i = 0; i < this.store.length; i++) {
+      const key = store.key(i);
+      ret[key] = this.getItem(key);
+    }
+
+    return ret;
+  }
+}
+
+const store = new Store("localStorage");
+export const session = new Store("sessionStorage");
+
+export default store;
--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -4,17 +4,31 @@
       <icon name="back" @click="$router.go(-1)" />
       <icon name="delete" v-if="word" @click="clear" />
 
-      <form>
+      <form @submit.prevent="search(word)">
         <input placeholder="搜索歌曲、歌手" id="input" v-model="word" />
       </form>
     </div>
 
-    <div class="search-normal">
+    <div class="search-normal" v-if="hots.length">
       <div class="hots-wrapper" v-if="hots.length">
         <div class="hots-title">热门搜索</div>
         <div class="hots-list">
           <div class="hot-item" v-for="hot in hots" :key="hot.first">
             {{ hot.first }}
+          </div>
+        </div>
+      </div>
+
+      <div class="historys-wrapper" v-if="historys.length">
+        <div class="historys-title">
+          搜索历史
+          <icon name="clear" @click="clearHistorys" />
+        </div>
+
+        <div class="historys-list">
+          <div class="history-item" v-for="history in historys" :key="history">
+            <icon name="clock" />
+            <span class="name">{{ history }}</span>
           </div>
         </div>
       </div>
@@ -25,11 +39,14 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import { getHots } from "../../api/search";
+import storage from "../../utils/storage";
+const SEARCH_HISTORYS = "SEARCH_HISTORYS";
 
 @Component
 export default class extends Vue {
   word: string = "";
   hots: any[] = [];
+  historys: string[] = storage.getItem(SEARCH_HISTORYS) || [];
 
   clear() {
     this.word = "";
@@ -40,6 +57,30 @@ export default class extends Vue {
     const res = await getHots();
 
     this.hots = res.data.result.hots;
+  }
+
+  pushHistory(word: string) {
+    const index = this.historys.indexOf(word);
+
+    if (index > -1) {
+      this.historys.splice(index, 1);
+    }
+
+    this.historys.unshift(word);
+    if (this.historys.length >= 10) {
+      this.historys.length = 10;
+    }
+
+    storage.setItem(SEARCH_HISTORYS, this.historys);
+  }
+
+  clearHistorys() {
+    this.historys = [];
+    storage.removeItem(SEARCH_HISTORYS);
+  }
+
+  async search(word: string) {
+    this.pushHistory(word);
   }
 
   async created() {
@@ -121,6 +162,40 @@ export default class extends Vue {
           margin: 0 10px 12px 0;
           position: relative;
           .active();
+        }
+      }
+    }
+
+    .historys-wrapper {
+      padding: 0 18px;
+      .historys-title {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: @font-size-m;
+        margin-bottom: 12px;
+
+        .icon-clear {
+          color: @text-color-2;
+          width: 18px;
+          height: 18px;
+        }
+      }
+
+      .historys-list {
+        .history-item {
+          display: flex;
+          align-items: center;
+          padding: 10px 0;
+
+          .icon-clock {
+            color: @text-color-2;
+          }
+
+          .name {
+            margin-left: 6px;
+            font-size: 15px;
+          }
         }
       }
     }
