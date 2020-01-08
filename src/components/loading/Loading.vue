2a8462bebbbba909e3330cc76d5a448302b9c82e
李培汉 <lipeihan@chandashi.com>
2020-01-08 10:11:18 +0800

    perf: 优化

--- src/components/loading/Loading.vue
+++ src/components/loading/Loading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="loading" v-if="show">
+  <div class="loading" v-show="show">
     <div class="loading-content">
       <img src="../../assets/images/loading2.gif" />
       <div class="message">{{ message }}</div>
